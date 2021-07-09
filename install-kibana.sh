c1d46ba98063f22d52769a3464aac1e5689130e1
otakterbelah <87186098+otakterbelah@users.noreply.github.com>
2021-07-09 19:08:12 +0700

    Create install-kibana.sh

---
+++ install-kibana.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#script install elastic search
+#update os
+sudo apt-get update
+apt-get upgrade -y
+apt-get install gnupg -y
+#install java
+sudo apt-get install default-jre -y
+printf "\n\n===========install java!===========\n\n"
+#cek java version
+java -version
+"\n\n===========java version===========\n\n"
+#import GPG-Key Elastic Stack
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+printf "\n\n===========Success Import GPG-Key!===========\n\n"
+#install http transport
+sudo apt-get install apt-transport-https -y
+printf "\n\n===========Success https transport===========\n\n"
+#input repo Elastic Stack 
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+printf "\n\n===========Success repo add===========\n\n"
+#update dan install kibana 
+sudo apt-get update && sudo apt-get install kibana -y
+printf "\n\n===========Success install kibana ===========\n\n"
